dc70d4775e6229a75bbd9718ffd99cc68e93a462
Dilorfin <pavel.kuybida@gmail.com>
2023-12-09 22:53:24 +0200

    Create basic Home design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<div class="container-fluid vh-100 d-flex flex-column justify-content-center align-items-center">
+		<h1 class="display-1 text-center mb-5">EVEN WIZARDS GO TO WORK</h1>
+
+		<div class="row">
+			<div class="d-flex justify-content-center">
+				<div class="input-group mb-3 flex-grow-1">
+					<input type="email" class="form-control" placeholder="Enter your email">
+					<button class="btn btn-outline-dark">Subscribe</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="d-flex justify-content-center mt-5">
+			<a href="https://itch.io/your-studio-name" target="_blank">
+				<img src="@/assets/icons/itchio.svg" alt="Itch.io" width="32">
+			</a>
+			<a href="https://discord.gg/your-discord-server" target="_blank" class="ms-3">
+				<img src="@/assets/icons/discord.svg" alt="Discord" width="32">
+			</a>
+			<a href="#" target="_blank" class="ms-3">
+				<img src="@/assets/icons/x-twitter.svg" alt="X (Twitter)" width="32">
+			</a>
+			<a href="https://t.me/your-telegram-channel" target="_blank" class="ms-3">
+				<img src="@/assets/icons/telegram.svg" alt="Telegram" width="32">
+			</a>
+		</div>
+	</div>
 </template>
